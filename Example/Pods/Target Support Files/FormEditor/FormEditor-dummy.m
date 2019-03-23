6f196f15f88bd7d81794786ba308f7db97c7a91e
Safronov Evgeniy <evsafronov.personal@yandex.ru>
2019-03-23 19:04:52 +0300

    Example's pods directory added

---
+++ Example/Pods/Target Support Files/FormEditor/FormEditor-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_FormEditor : NSObject
+@end
+@implementation PodsDummy_FormEditor
+@end
---
+++ Example/Pods/Target Support Files/Pods-FormEditor_Example/Pods-FormEditor_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FormEditor_Example : NSObject
+@end
+@implementation PodsDummy_Pods_FormEditor_Example
+@end
